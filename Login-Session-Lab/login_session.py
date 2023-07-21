93bbfe928604b77281aa59bf96e2f6ee243c0776
almaro24-meet <110034583+almaro24-meet@users.noreply.github.com>
2023-07-21 20:49:26 +0300

    #Working...

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -4,10 +4,26 @@ from flask import session as login_session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
+@app.route('/', methods=['GET', 'POST']) # What methods are needed?
 def home():
-	
-	return render_template('home.html')
+    if request.method == 'POST':
+        try:
+            quote = request.form['quote']
+            age = request.form['age']
+            author = request.form['author']
+            login_session['quote'] = str(quote)
+            login_session['age'] = int(age)
+            login_session['author'] = str(author)
+
+            if quote and age and author:
+                return render_template('thanks.html')
+
+        except (KeyError, ValueError):
+            return redirect(url_for('error'))
+    else:
+    	return render_template('home.html')
+
+
 
 
 @app.route('/error')
@@ -18,8 +34,10 @@ def error():
 
 @app.route('/display')
 def display():
-
-	return render_template('display.html', ) # What variables are needed?
+	q = login_session['quote']
+	a = login_session['age']
+	a2 = login_session['author']
+	return render_template('display.html', q = q, a = a, a2 = a) # What variables are needed?
 
 
 @app.route('/thanks')
